89624857dc45d297c7ddb5641348e166ce980535
ingrid-2154 <78661887+ingrid-2154@users.noreply.github.com>
2023-07-14 17:45:52 +0000

    Adicionando a função de continuar o jogo até acertar o número.

--- JS/validacao.js
+++ JS/validacao.js
@@ -1,17 +1,32 @@
 function   verificaSeOChutePossuiValorValido(chute){
-  const numero = +chute
+const numero = +chute
 
-  if (chuteForInvalido(numero)){
-      console.log('valor inválido')
-  }
+    if (chuteForInvalido(numero)){
+      elementoChute.innerHTML += '<div>valor inválido</div>'
+      return
+    }
+    if(numeroForMaiorOuMenorQueOValorPermitido(numero))
+      elementoChute.innerHTML +=`
+      <div>valor inválido: Fale um número entre ${menorValor} e       
+      ${maiorValor} </div>`
+    }
+    if(numero === numeroSecreto) {
+      documento.body.innerHTML = `
+      <h1>Você acertou!</h1>
+      <h3> O número secreto era ${numeroSecreto}`
+    } else if ( numero > numeroSecreto){
+      elementoChute.innerHTML += `
+      <div> O número secreto é menor<i class="fa-solid fa-down=long"></i></div>
+      ` 
+    } else {
+      elementoChute.innerHTML += `
+      <div> O número secreto é maior<i class="fa-solid fa-up=long"></i></div>
+      ` 
+    }
 
-  if(numeroForMaiorOuMenorQueOValorPermitido(numero))
-      console.log('valor inválido: o número precisa estar entre ${menorValor} e ${maiorValor}')
-    
-}
 function chuteForInvalido(numero) {
     return Number.isNaN(numero)  
-}
+  }
 
 function numeroForMaiorOuMenorQueOValorPermitido(numero)
-  return  numero > maiorValor || numero < menorValor
+   return  numero > maiorValor || numero < menorValor
